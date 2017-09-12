6462ccdd32128d6a3be30aaf329a704a91ec4565
Julien Levasseur <dko2327@lab-vfbl5831-0.mvt.desjardins.dev>
2017-09-12 17:07:11 -0400

    add openstack provider

---
+++ openstack/provider/main.tf
@@ -0,0 +1,15 @@
+# export TF_VAR_username=""
+# export TF_VAR_tenant_name=""
+# export TF_VAR_password=""
+# export TF_VAR_auth_url="
+# export TF_VAR_region=""
+# export OS_PROJECT_NAME=""
+# export OS_USER_DOMAIN_NAME=""
+
+provider "openstack" {
+  user_name   = "${var.username}"
+  tenant_name = "${var.tenant_name}"
+  password    = "${var.password}"
+  auth_url    = "${var.auth_url}"
+  region      = "${var.region}"
+}
