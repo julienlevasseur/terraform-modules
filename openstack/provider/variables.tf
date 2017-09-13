fed50fad04f9786bdf57d17d247933b61652ce9a
Julien Levasseur <dko2327@lab-vfbl5831-0.mvt.desjardins.dev>
2017-09-13 16:30:17 -0400

    revert params

--- openstack/provider/variables.tf
+++ openstack/provider/variables.tf
@@ -1,22 +1,5 @@
-#variable "username" {}
-#variable "tenant_name" {}
-#variable "password" {}
-#variable "auth_url" {}
-#variable "region" {}
-
-variable "auth_url" {}
-variable "region" {}
-variable "user_name" {}
-variable "user_id" {}
-variable "tenant_id" {}
+variable "username" {}
 variable "tenant_name" {}
 variable "password" {}
-variable "token" {}
-variable "domain_id" {}
-variable "domain_name" {}
-variable "insecure" {}
-variable "cacert_file" {}
-variable "cert" {}
-variable "key" {}
-variable "endpoint_type" {}
-variable "swauth" {}
+variable "auth_url" {}
+variable "region" {}
