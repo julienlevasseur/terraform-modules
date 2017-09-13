64416627066a2c19a5198cafb82f20abd18eaa8f
Julien Levasseur <dko2327@lab-vfbl5831-0.mvt.desjardins.dev>
2017-09-13 17:03:50 -0400

    add openstack instance

---
+++ openstack/instance/variables.tf
@@ -0,0 +1,7 @@
+variable "name" {}
+variable "image_name" {}
+variable "flavor_name" {}
+variable "key_pair" {}
+variable "security_groups" {}
+variable "network" {}
+variable "availability_zone" {}
