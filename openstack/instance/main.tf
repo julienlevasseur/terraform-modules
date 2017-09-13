de38c70eafd4b908638425acc40b7084fe5a3fd0
Julien Levasseur <dko2327@lab-vfbl5831-0.mvt.desjardins.dev>
2017-09-13 17:14:19 -0400

    correct network definition

--- openstack/instance/main.tf
+++ openstack/instance/main.tf
@@ -4,5 +4,7 @@ resource "openstack_compute_instance_v2" "instance" {
   flavor_name       = "${var.flavor_name}"
   key_pair          = "${var.key_pair}"
   security_groups   = "${var.security_groups}"
-  network           = "${var.network}"
+  network {
+    name = "${var.network}"
+  }
 }
