b05519760c96c6519fb6935cb0e0c8981ca5193b
Julien Levasseur <dko2327@lab-vfbl5831-0.mvt.desjardins.dev>
2017-09-14 10:34:25 -0400

    correct secgroup in os instance

--- openstack/instance/main.tf
+++ openstack/instance/main.tf
@@ -3,8 +3,19 @@ resource "openstack_compute_instance_v2" "instance" {
   image_name        = "${var.image_name}"
   flavor_name       = "${var.flavor_name}"
   key_pair          = "${var.key_pair}"
-  security_groups   = "${var.security_groups}"
+  security_groups   = ["${var.security_groups}"]
   network {
     name = "${var.network}"
   }
 }
+
+# Note :
+#   security_groups is embeded in brackets to avoid this error :
+#     * module.openstack_instance.openstack_compute_instance_v2.instance:
+#        security_groups: should be a list
+#
+#   To append elements to this list use :
+#     security_groups   = "${list(module.openstack_secgroup_ssh.foo)}"
+#   or :
+#     security_groups   = "${concat(module.openstack_secgroup_ssh.foo, module.openstack_secgroup_ssh.bar)}"
+#
