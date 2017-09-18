8345c5a766a1a4ca50bb1f4771d46ddaa10efb1c
Julien Levasseur <dko2327@lab-vfbl5831-0.mvt.desjardins.dev>
2017-09-18 13:24:04 -0400

    adding output to vol and vol attach

--- openstack/volume/main.tf
+++ openstack/volume/main.tf
@@ -4,3 +4,51 @@ resource "openstack_blockstorage_volume_v2" "volume" {
   description = "${var.description}"
   size        = "${var.size}"
 }
+
+output "id" {
+  value = "${openstack_blockstorage_volume_v2.volume.id}"
+}
+
+output "region" {
+  value = "${openstack_blockstorage_volume_v2.volume.region}"
+}
+
+output "size" {
+  value = "${openstack_blockstorage_volume_v2.volume.size}"
+}
+
+output "name" {
+  value = "${openstack_blockstorage_volume_v2.volume.name}"
+}
+
+output "description" {
+  value = "${openstack_blockstorage_volume_v2.volume.description}"
+}
+
+output "availability_zone" {
+  value = "${openstack_blockstorage_volume_v2.volume.availability_zone}"
+}
+
+output "image_id" {
+  value = "${openstack_blockstorage_volume_v2.volume.image_id}"
+}
+
+output "source_vol_id" {
+  value = "${openstack_blockstorage_volume_v2.volume.source_vol_id}"
+}
+
+output "snapshot_id" {
+  value = "${openstack_blockstorage_volume_v2.volume.snapshot_id}"
+}
+
+output "imetadata {
+  value = "${openstack_blockstorage_volume_v2.volume.metadata}"
+}
+
+output "volume_type" {
+  value = "${openstack_blockstorage_volume_v2.volume.volume_type}"
+}
+
+output "attachement" {
+  value = "${openstack_blockstorage_volume_v2.volume.attachement}"
+}
--- openstack/volume_attach/main.tf
+++ openstack/volume_attach/main.tf
@@ -2,3 +2,15 @@ resource "openstack_blockstorage_volume_attach_v2" "volume_attach" {
   volume_id = "${var.volume_id}"
   host_name = "${var.host_name}"
 }
+
+output "data" {
+  value = "${openstack_blockstorage_volume_attach_v2.volume.data}"
+}
+
+output "driver_volume_type" {
+  value = "${openstack_blockstorage_volume_attach_v2.volume.driver_volume_type}"
+}
+
+output "mount_point_base" {
+  value = "${openstack_blockstorage_volume_attach_v2.volume.mount_point_base}"
+}
